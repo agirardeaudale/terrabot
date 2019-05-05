fe68de50f53e6fa9a773e5623a121721be1a7d91
Andrew Girardeau-Dale <agirardeaudale@users.noreply.github.com>
2019-05-05 19:20:42 -0400

    Add Alchemists VP -> Coin conversion rate

--- terrabot/sim/data/factions.py
+++ terrabot/sim/data/factions.py
@@ -1,3 +1,12 @@
+from terrabot.sim.map import Terrain
 from terrabot.sim.player import Faction
+from terrabot.sim.resources import ResourceType, _DEFAULT_CONVERSION_RATES
 
-FACTIONS = tuple( )
+FACTIONS = tuple(
+        Faction(
+                name = "Alchemists",
+                home_terrain = Terrain.SWAMP,
+                resource_conversion_rates = frozendict_with_item(
+                        _DEFAULT_CONVERSION_RATES, (ResourceType.POINTS, ResourceType.COINS), 1)
+                )
+        )
